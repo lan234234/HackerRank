bf903e746ffd11d4b9d21a7da3dc9a67c79df0b0
Lan <lan.peng234@gmail.com>
2024-12-23 11:59:54 -0700

    update MinSwapsToMakePalindrome

--- src/amazon/MinSwapsToMakePalindrome.java
+++ src/amazon/MinSwapsToMakePalindrome.java
@@ -1,25 +1,45 @@
 package amazon;
 
+/**
+ * Minimum Swaps to Make Palindrome
+ * You are given a binary string, s, consisting of characters '0' and '1'.
+ * Transform this string into a palindrome by performing some operations.
+ * In one operation, swap any two characters, s[i] and s[j].
+ * Determine the minimum number of swaps required to make the string a palindrome.
+ * If it is impossible to do so, then return -1.
+ *
+ * Note: A palindrome is a string that reads the same backward as forward,
+ * for example, strings "0", "111", "010", "10101" are palindromes,
+ * but strings "001", "10", "11101" are not.
+ *
+ * Example 1:
+ *
+ * Input:  s = "0100101"
+ * Output: 2
+ *
+ */
 public class MinSwapsToMakePalindrome {
     public int minSwapsToMakePalindrome(String s) {
         // corner case:
         if (s == null || s.length() <= 1)   return 0;
         // general case:
         int result = 0;
-        int i = 0;
-        int j = s.length() - 1;
+        int left = 0;
+        int right = s.length() - 1;
+        // needSwap represents whether we need to find another bit to swap with
+        // one of the current bits between s.charAt(i) and s.charAt(j)
         boolean needSwap = false;
-        while (i < j) {
-            if (s.charAt(i) != s.charAt(j)) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
                 if (!needSwap) {
                     result++;
                 }
                 needSwap = !needSwap;
             }
-            i++;
-            j--;
+            left++;
+            right--;
         }
-        if (i == j) return result;
+        if (left == right) return result;
         return needSwap ? -1 : result;
     }
 }
