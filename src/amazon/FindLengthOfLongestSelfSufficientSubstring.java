00a64dd3377f3c9abab011d37f2148945e69468f
Lan <lan.peng234@gmail.com>
2025-01-05 10:30:06 -0700

    FindLengthOfLongestSelfSufficientSubstring

--- src/amazon/FindLengthOfLongestSelfSufficientSubstring.java
+++ src/amazon/FindLengthOfLongestSelfSufficientSubstring.java
@@ -1,5 +1,9 @@
 package amazon;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class FindLengthOfLongestSelfSufficientSubstring {
 
     /**
@@ -7,7 +11,7 @@ public class FindLengthOfLongestSelfSufficientSubstring {
      * test cases: s: "amazomsarvicesa" -> 5 ("rvice")
      * a mazomsarvicesa
      */
-    public int findLengthOfLongestSelfSufficientSubstring(String s) {
+    public int solution1(String s) {
         int n = s.length();
         if (n <= 1) return 0; // A proper substring can't be the entire string
 
@@ -44,5 +48,87 @@ public class FindLengthOfLongestSelfSufficientSubstring {
         }
 
         return maxLen;
+    }
+
+    /**
+     * method 2: sliding window
+     */
+    public int solution2(String s) {
+        int res = -1;
+
+        // left and right are used to store the leftmost and right most index of the character
+        int[] left = new int[26];
+        Arrays.fill(left, -1);
+        int[] right = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            int index = s.charAt(i) - 'a';
+            if (left[index] == -1) {
+                left[index] = i;
+            }
+            right[index] = i;
+        }
+
+        // a self-sufficient substring must start with the leftmost of char1 and
+        // end with the right most of char2, and char1 and char2 can be the same
+        // try all possible (char1, char2) pair -> valid each character
+        for (int i = 0; i < 26; i++) {  //  (char) (i + 'a') represent the char1
+            if (left[i] == -1)  continue;
+            int left1 = left[i];
+            int right1 = right[i];
+            int end = right1;   // end of the substring should >= right1
+            for (int j = left1; j < s.length(); j++) {  // only consider the char after c1 in s
+                if (left1 == 0 && j == s.length() - 1)  continue;   // skip full string
+                int index = s.charAt(j) - 'a';
+                int left2 = left[index];
+                int right2 = right[index];
+                end = Math.max(end, right2);
+                if (left2 < left1)  break;  // invalid substring, no need to continue
+                if (j == end) {
+                    res = Math.max(res, right2 - left1 + 1);
+                }
+            }
+        }
+        return res;
+    }
+
+    /**
+     * method 3: try all possible (c1, c2) pair
+     */
+    public int solution3(String s) {
+        int res = -1;
+
+        // left and right are used to store the leftmost and right most index of the character
+        int[] left = new int[26];
+        Arrays.fill(left, -1);
+        int[] right = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            int index = s.charAt(i) - 'a';
+            if (left[index] == -1) {
+                left[index] = i;
+            }
+            right[index] = i;
+        }
+
+        // a self-sufficient substring must start with the leftmost of char1 and
+        // end with the right most of char2, and char1 and char2 can be the same
+        // try all possible (char1, char2) pair -> valid each character
+        for (int i = 0; i < 26; i++) {  //  (char) (i + 'a') represent the char1
+            if (left[i] == -1)  continue;
+            int left1 = left[i];
+            int right1 = right[i];
+            int end = right1;   // end of the substring should >= right1
+            for (int j = left1; j < s.length(); j++) {  // only consider the char after c1 in s
+                if (left1 == 0 && j == s.length() - 1)  continue;   // skip full string
+                int index = s.charAt(j) - 'a';
+                int left2 = left[index];
+                int right2 = right[index];
+                end = Math.max(end, right2);
+                if (left2 < left1)  break;  // invalid substring, no need to continue
+                if (j == end) {
+                    res = Math.max(res, right2 - left1 + 1);
+                }
+            }
+        }
+        return res;
     }
 }
