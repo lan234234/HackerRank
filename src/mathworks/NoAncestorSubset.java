5a79f3cd402ea06470a11787c9c5becde0338a51
Lan <lan.peng234@gmail.com>
2025-02-03 17:17:11 -0700

    NoAncestorSubset

--- src/mathworks/NoAncestorSubset.java
+++ src/mathworks/NoAncestorSubset.java
@@ -1,52 +1,27 @@
 package mathworks;
 
+import javax.swing.tree.TreeNode;
 import java.util.ArrayList;
 import java.util.List;
 
 public class NoAncestorSubset {
-    List<List<Integer>> tree;
-    int[] weights;
-    int[][] dp;
     public int solution(int treeNodes, int[] treeFrom, int[] treeTo, int[] weight) {
-        // Convert to 1-based index
-        weights = new int[treeNodes + 1];
-        for (int i = 0; i < treeNodes; i++) {
-            weights[i + 1] = weight[i];
-        }
-
-        // Build tree
-        tree = new ArrayList<>();
-        for (int i = 0; i <= treeNodes; i++) {
+        List<List<Integer>> tree = new ArrayList<>();
+        for (int i = 0; i < treeNodes + 1; i++) {
             tree.add(new ArrayList<>());
         }
         for (int i = 0; i < treeFrom.length; i++) {
             tree.get(treeFrom[i]).add(treeTo[i]);
-            tree.get(treeTo[i]).add(treeFrom[i]); // Bidirectional for undirected tree
         }
 
-        // DP array, where dp[node][0] = max sum excluding node, dp[node][1] = max sum including node
-        dp = new int[treeNodes + 1][2];
-
-        // Run DFS from root (node 1) with no parent (-1)
-        dfs(1, -1);
-
-        // Maximum sum of a valid subset
-        return Math.max(dp[1][0], dp[1][1]);
+        return getMaxSubsetSum(tree, 1, weight);
     }
 
-    private void dfs(int node, int parent) {
-        dp[node][1] = weights[node]; // If we include this node
-
+    private int getMaxSubsetSum(List<List<Integer>> tree, int node, int[] weight) {
+        int childSum = 0;
         for (int child : tree.get(node)) {
-            if (child == parent) continue; // Avoid going back to parent
-
-            dfs(child, node);
-
-            // If we don't include 'node', we can take max of both states of child
-            dp[node][0] += Math.max(dp[child][0], dp[child][1]);
-
-            // If we include 'node', we must exclude child
-            dp[node][1] += dp[child][0];
+            childSum += getMaxSubsetSum(tree, child, weight);
         }
+        return Math.max(childSum, weight[node - 1]);
     }
 }
